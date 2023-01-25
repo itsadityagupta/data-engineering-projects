19608cc3fe718b0f278bc4ef6bf594646ac7ca48
Aditya-Gupta1 <guptaaditya008@gmail.com>
2023-01-25 21:21:33 +0530

    remove other projects

--- elt-dbt/transformations/models/staging/stg_churn__customer_details.sql
+++
@@ -1,4 +0,0 @@
-with final as (
-    select * from {{ source('churn', 'customer_details') }}
-)
-select * from final
--- elt-dbt/transformations/models/staging/stg_churn__population.sql
+++
@@ -1,4 +0,0 @@
-with final as (
-    select * from {{ source('churn', 'population') }}
-)
-select * from final
